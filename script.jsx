4560bed5ad6f4b5cd72fd77f87ef7d4e9434dde0
Mario Apra <mario_surfing@hotmail.com>
2017-09-28 20:50:10 +1300

    Lesson 4 - Reusable Components

--- script.jsx
+++ script.jsx
@@ -16,9 +16,33 @@ var Product = React.createClass({
           <p> Android - $199</p>
           <button onClick={this.buy}>Buy</button>
           <h3>Qty: {this.state.qty} item(s)</h3>
+          <hr/>
       </div>
       );
   }
 });
 
-React.render(<Product/>, document.getElementById("root"));
+var Total = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h3>Total Cash:  </h3>
+      </div>
+    )
+  }
+});
+
+var ProductList = React.createClass({
+  render: function(){
+    return (
+        <div>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Total/>
+        </div>
+    )
+  }
+});
+
+React.render(<ProductList/>, document.getElementById("root"));
